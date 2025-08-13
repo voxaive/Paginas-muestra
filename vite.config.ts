b49d0322f6d34dc9825f23a1713f9d636cea3f84
Laureano Furno <laureanofurno@gmail.com>
2025-08-12 22:29:00 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Paginas-muestra/', 
+  base: "https://Paginas-muestra.github.io/Paginas-muestra/", 
 });
