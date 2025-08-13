16770f0a2c5f2ace3a57cc6e004e922129ec3272
Paginas-muestra <practicascorreo1@gmail.com>
2025-08-12 21:41:22 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://paginas-muestra/",
+  base: "https://paginas-muestra.github.io",
 })
